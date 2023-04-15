2c20e7ce2294e46d4948b1879140ec065bd244b1
Yalikun Yilida <eldar940818@gmail.com>
2023-04-15 04:06:51 +0200

    amount control

--- src/components/productpage/ProductRight.jsx
+++ src/components/productpage/ProductRight.jsx
@@ -1,12 +1,24 @@
 import styles from '../../assets/styles/productpage/product.module.css'
 import {  BsCartFill } from 'react-icons/bs'
+import { useState } from 'react'
 
 const ProductRight = (props) => {
+
+    const [amount, setAmount] = useState(1)
+
+    const handleIncrease = () => {
+        setAmount(amount + 1)
+    }
+
+    const handleDecrease = () => {
+        setAmount(amount > 1 ? amount - 1 : 1)
+    }
+
   return (
     <div className={styles.right}>
         <article className={styles.iteminfo}>
             <h2>{props.title}</h2>
-            <h4>{props.price} Kr/-</h4>
+            <h4>{props.price * amount} Kr/-</h4>
             <p>{props.description}</p>
             <select id="size" className={styles.size}>
                 {props.sizes.map((opt, index) => (
@@ -14,8 +26,17 @@ const ProductRight = (props) => {
                 ))}
             </select>
             <div className={styles.amount}>
-                <label>Anount:</label>
-                <input type="number" />
+                <button
+                    onClick={handleDecrease}
+                >
+                    -
+                </button>
+                <span>{amount}</span>
+                <button
+                    onClick={handleIncrease}
+                >
+                    +
+                </button>
             </div>
             <button className={styles.addcart}>
                 <span>Add to cart</span>
